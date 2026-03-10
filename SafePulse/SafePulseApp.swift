5b54bc37caea9ce3cfe11d7de4afe4de4bcc27c0
hardikaregmi <hardika.regmi123@gmail.com>
2026-03-10 12:23:53 -0500

    Initial Commit

---
+++ SafePulse/SafePulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafePulseApp.swift
+//  SafePulse
+//
+//  Created by Hardika Regmi on 3/10/26.
+//
+
+import SwiftUI
+
+@main
+struct SafePulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
